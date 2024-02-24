e09ff411f6912d602dd24412794f9b4b028489e1
Matthew <96619454+LichasX@users.noreply.github.com>
2024-02-24 13:52:39 +0800

    Update and rename

--- TicTacToe/202402_TicTacToe_v2.py
+++ TicTacToe/202402_TicTacToe_v2.py
@@ -24,8 +24,7 @@ def turn_rotation(game_round):
         return [game_round, 2]
 
 
-def win_check():
-    global end_condition, flag
+def win_check(end_condition):
     new_board = []
     if game_key[0] == board_size[0] * board_size[1]:  # all squares are filled
         end_condition = 2
@@ -53,6 +52,10 @@ def win_check():
                     flag = "X"
                 elif new_board[y_value][x_value] == "O":
                     flag = "O"
+    if end_condition == 1:
+        return [end_condition, flag]
+    else:
+        return [end_condition]
 
 
 while not end_condition:
@@ -61,7 +64,7 @@ while not end_condition:
     while True:
         move_coord = str(input(f"Round:{game_key[0]}\nPlayer {game_key[1]}, enter your move in the form of coordinates xy where x is the column and y is the row your move is at. "))
         move_board = (int(move_coord[1])-1) * board_size[0] + (int(move_coord[0])-1) # converts coordinate form of move to index form to be used in board list
-        if board[move_board] == " ":
+        if board[move_board] == " ": #only breaks loop if valid move
             break
         else:
             print("Error. Enter valid move.")
@@ -69,13 +72,14 @@ while not end_condition:
         board[move_board] = "X"
     else:
         board[move_board] = "O"
-    win_check()
+    win_check_list = win_check(end_condition)
+    end_condition = win_check_list[0]
 
 print_board()
 if end_condition == 2:
     print("Draw")
 elif end_condition == 1:
-    if flag == "X":
+    if win_check_list[1] == "X":
         print("Player 1 has won.")
-    elif flag == "O":
+    elif win_check_list[1] == "O":
         print("Player 2 has won.")
