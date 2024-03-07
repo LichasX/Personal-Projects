f98b0f6b16d70267092797c3aad248d3d2969a8e
Matthew <96619454+LichasX@users.noreply.github.com>
2024-03-07 14:41:34 +0800

    lunchbox.py

---
+++ codebreaker/lunchbox.py
@@ -0,0 +1,13 @@
+inputs = str(input("")).split() #(n, m)
+lst = []
+remaining_val = int(inputs[0])
+count = 0
+for m in range(int(inputs[1])):
+    k = int(input(""))
+    lst.append(int(k))
+while remaining_val > 0 and len(lst) > 0 and not(min(lst) > remaining_val):
+    remaining_val -= min(lst)
+    lst.remove(min(lst))
+    count += 1
+print(count)
+    
