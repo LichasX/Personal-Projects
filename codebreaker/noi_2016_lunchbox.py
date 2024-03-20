14484b8b5fa654dd81f2cbd200702d8cbe2e2ac9
Matthew <96619454+LichasX@users.noreply.github.com>
2024-03-20 15:18:33 +0800

    Update noi_2016_lunchbox.py

--- codebreaker/noi_2016_lunchbox.py
+++ codebreaker/noi_2016_lunchbox.py
@@ -1,13 +1,12 @@
-# https://codebreaker.xyz/problem/lunchbox
-inputs = str(input("")).split() #(n, m)
-lst = []
+# https://codebreaker.xyz/problem/lunchbox, tle by 0.51s from 0.5s
+inputs = str(input()).split() #(n, m)
 remaining_val = int(inputs[0])
 count = 0
-for m in range(int(inputs[1])):
-    k = int(input(""))
-    lst.append(int(k))
-while remaining_val > 0 and len(lst) > 0 and not(min(lst) > remaining_val):
-    remaining_val -= min(lst)
-    lst.remove(min(lst))
-    count += 1
+lst = [int(input()) for m in range(int(inputs[1]))]
+x = min(lst)
+while remaining_val > 0 and len(lst) > 0 and not(x > remaining_val):
+  x = min(lst)
+  remaining_val -= x
+  lst.remove(x)
+  count += 1
 print(count)
