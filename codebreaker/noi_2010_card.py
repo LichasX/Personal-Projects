0958a95af7a79a26334400ad8e7a55dd572e98d1
Matthew <96619454+LichasX@users.noreply.github.com>
2024-03-20 15:29:01 +0800

    Update and rename card.py to noi_2010_card.py

--- codebreaker/noi_2010_card.py
+++ codebreaker/noi_2010_card.py
@@ -1,11 +1,9 @@
-inputs = str(input("")).upper().split()
+#https://codebreaker.xyz/problem/card, tle 1s from 1s
+inputs = str(input()).split()
 cards = [x for x in range(int(inputs[0]))]
 for move in inputs[2]:
     if move == "A": #card top to bottom
-        cards.append(cards[0])
-        del cards[0]
+        cards.append(cards.pop(0))
     elif move == "B": #card 2nd top to bottom
-        cards.append(cards[1])
-        del cards[1]
+        cards.append(cards.pop(1))
 print(cards[int(inputs[1]) -1], cards[int(inputs[1])], cards[int(inputs[1]) + 1])
-    
